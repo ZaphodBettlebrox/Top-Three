54de2234418e44d04b71344e47e217fecd77860c
Gregory Reneris <greg@reneris.com>
2020-01-22 13:44:53 -0800

    added rec route page.

---
+++ routes/rec-routes.js
@@ -0,0 +1,37 @@
+var db = require("../models");
+
+
+// 
+
+// get all existing top 3 routes.
+// render all existing top 3 informations through handlebars.
+// 
+
+module.exports = function(app) {
+    app.get("/api/category", function(req, res) {
+      // 1. Add a join to include all of each Author's Posts
+      db.List.findAll({}).then(function(dbList) {
+        res.json(dbList);
+        res.render(test.html);
+      });
+    });
+
+    app.post("/api/category", function(req, res) {
+    db.List.create(req.body).then(function(dbList) {
+        res.json(dbList);
+        });
+    });
+
+    app.delete("/api/category/:id", function(req, res) {
+        db.List.destroy({
+            where: {
+            id: req.params.id
+            }
+        }).then(function(dbList) {
+            res.json(dbList);
+        });
+    });
+
+    
+
+};
