ca42863b5599a5d30bad2aa2d2c9a769d77db4b2
TysonVonnBeck <54685544+tysonvonnbeck@users.noreply.github.com>
2020-01-23 11:59:36 -0800

    added ajax for html categories

---
+++ public/js/top3.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    $(".category").on("click", function(event) {
+        event.preventDefault()
+      var id = $(this).attr("id");
+        console.log(id);
+    
+    });
+
+    });
+  
+    
