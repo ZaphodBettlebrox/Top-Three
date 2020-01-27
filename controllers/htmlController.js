06c69366664676ca74f3d6edb94441380d3778ef
Sarah <sarkwon89@gmail.com>
2020-01-27 00:14:06 -0800

    updated nav bar, get data for final view a user's rec page and render page

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,28 +1,45 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
 
 
 //first page that will load.  If user has account and logs in it will take them to the entry page.
-router.get("/",function(req,res){
-    if(req.session.user) {
-        res.render("entry",req.session.user);
-    }else {
+router.get("/", function (req, res) {
+    if (req.session.user) {
+        res.render("entry", req.session.user);
+    } else {
         res.render("login");
     }
 })
 
 
+
 //render entry page
-router.get("/entrypage", function(req, res){
-    res.render("entry")
-})
+router.get("/entrypage", function (req, res) {
+    if (req.session.user) {
+        res.render("entry", req.session.user);
+    } else {
+        // alert("You need to login")
+        res.render("login");
+    }
+});
 
 //render viewUserRec html
-router.get("/allUserRec", function(req,res){
-    
-    res.render("allUserRec")
-})
+router.get("/userRec", function (req, res) {
+    if (req.session.user) {
+        res.render("userRec", req.session.user);
+    } else {
+        // alert("You need to login")
+        res.render("login");
+    }
+});
 
+//render viewUserRec html
+router.get("/alluserRec", function (req, res) {
+    if (req.session.user) {
+        res.render("alluserRec", req.session.user);
+    } else {
+        res.render("login");
+    }
+});
 
 module.exports = router
-
