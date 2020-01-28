07d674b42e81f47759d3cf52b1932e4e6cbc00e5
Gregory Reneris <greg@reneris.com>
2020-01-27 19:23:26 -0800

    re-added a commented out line in top3.js

--- public/js/top3.js
+++ public/js/top3.js
@@ -42,19 +42,19 @@ $(document).ready(function () {
 });
 
 
-// //Get the button:
-// const mybutton = document.getElementById("myBtn");
+//Get the button:
+const mybutton = document.getElementById("myBtn");
 
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
